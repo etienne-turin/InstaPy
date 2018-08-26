f8762e1fc7d9a0bd94a6526c6dd2aafc86b4c023
etienne <etienne.turin@etu.univ-amu.fr>
2018-08-26 15:05:51 +0200

    1er session de test

--- quickstart.py
+++ quickstart.py
@@ -6,8 +6,8 @@ from selenium.common.exceptions import NoSuchElementException
 
 from instapy import InstaPy
 
-insta_username = ''
-insta_password = ''
+insta_username = '__amandinelbe__'
+insta_password = 'ficelle123'
 
 # set headless_browser=True if you want to run InstaPy on a server
 
@@ -25,20 +25,35 @@ try:
     session.login()
 
     # settings
+    session.set_dont_include(['rogissartquentin', 'claaumur', 'dextrocardie', 'violaine_f', 'mayazetoune', 'liamtbrg', 'carochoupinette', 'guigione13', 'friend3', 'baptistekenny',
+                              'max_scht', 'lucaaaasgarciarota', 'karakinian', 'cazorlacaroline', 'asolbb', 'lea_gndo', 'aurelkah', 'mado.jc', 'victoireberaud', 'gaelvandecasteele',
+                              'clem96e', 'melineboyer', 'mahee_daan', 'antoinens_', 'manukyan_._', 'florent.c83', 'marina.der', 'inesorfeo', 'joanna_bel', 'meloduff', 'myr_oceanne',
+                              'k_r_n_m_a', 'lucaas_bernardi13', 'colomet.dielo', 'sourire_a_la_vie_asso'])
     session.set_relationship_bounds(enabled=True,
 				 potency_ratio=-1.21,
 				  delimit_by_numbers=True,
-				   max_followers=4590,
-				    max_following=5555,
-				     min_followers=45,
+				   max_followers=10000,
+				    max_following=4000,
+				     min_followers=33,
 				      min_following=77)
-    session.set_do_comment(True, percentage=10)
-    session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
-    session.set_dont_include(['friend1', 'friend2', 'friend3'])
-    session.set_dont_like(['pizza', 'girl'])
+    session.set_do_comment(True, percentage=30)
+    session.set_comments(['Cool !', 'So much fun!!', 'Nicey!', 'Magic', ':)', 'Trop bien =D'])
+    session.set_dont_like(['sexy', 'sex', 'porno', 'gore', 'xxx'])
 
     # actions
-    session.like_by_tags(['natgeo'], amount=1)
+    session.like_by_tags(['nature','geographie','velo','voyage','amoureux','gite','ferme','tente', 'foodporn','patisserie','cuisine','restaurant','entreprise','france','animaux',
+                          'soleil','valise','bagages','mariage','robe de mariee','decoration','made in france','savon','eurovelo','marseille','combi','box','plamte','ecologie',
+                          'durable','paille en bambou','printemps','france','espagne','bicyclette','portugal','homemade','allemagne','belgique','pays-bas','finlande','norvege',
+                          'suede','lapponie','lituanie','lettonie','lestonie','grece','pologne','italie','fleur','plante','tombola','association','caritatif','cancer','enfants',
+                          'jeunes','lifestyle','boutique','boisson','cocktails','fruits','alcool','charcuterie','fromage','barbecue','brochette','hotel','chambre','bebe','savon','vacances','bougies'], amount=60)
+
+    # default enabled=False, follows ~ 10% of the users from the images, times=1
+    # (only follows a user once (if unfollowed again))
+    session.set_do_follow(enabled=True, percentage=30, times=2)
+
+    # unfollow users followed by bot
+    session.unfollow_users(amount=10, InstapyFollowed=(True, "nonfollowers"), style="FIFO", unfollow_after=90*60*60, sleep_delay=501)
+
 
 except Exception as exc:
     # if changes to IG layout, upload the file to help us locate the change
